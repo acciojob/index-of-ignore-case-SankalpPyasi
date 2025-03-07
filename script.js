0551c934052f3daecfa55f8c5ba6522d83a93769
SankalpPyasi <sankalppyasi6@gmail.com>
2025-03-07 21:42:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+    if (subStr === "") {
+        return 0; // Convention: empty substring is always found at index 0
+    }
+    if (str === "") {
+        return -1; // Empty string can't contain any non-empty substring
+    }
+
+    // Case-insensitive search using lowercased strings
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
+
+    return lowerStr.indexOf(lowerSubStr);
 }
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// Function to run the search and display result in the page
+function runSearch() {
+    const mainStr = document.getElementById("mainStr").value;
+    const subStr = document.getElementById("subStr").value;
+    const result = indexOfIgnoreCase(mainStr, subStr);
+
+    document.getElementById("result").innerText = `Index: ${result}`;
+}
+
+// Examples for console testing (you can remove this if unnecessary)
+console.log(indexOfIgnoreCase("Hello World", "world")); // 6
+console.log(indexOfIgnoreCase("apple", "Ple"));         // 2
+console.log(indexOfIgnoreCase("test", "aaa"));          // -1
